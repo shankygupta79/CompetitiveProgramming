6ee5efcb967564d294990f03e4c1e7b0698b20f2
shankygupta79 <shankygupta79@gmail.com>
2019-08-13 18:48:14 +0530

    Binary Tree DS CPP

--- Binary_Tree.cpp
+++ Binary_Tree.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include<queue>
 using namespace std;
-
+int distan=0;
 class node{
     public:
         int data;
@@ -127,6 +127,54 @@ int count(node*root){
     }
     return 1 + count(root->left) + count(root->right);
 }
+//Dist(n1, n2) = Dist(root, n1) + Dist(root, n2) - 2*Dist(root, lca)
+node *LCA(struct node* root, int n1, int n2){
+    if (root == NULL){
+        return NULL;
+    }
+    if (root->data==n1 || root->data == n2){
+        return root;
+    }
+    node *l  = LCA(root->left, n1, n2);
+    node *r = LCA(root->right, n1, n2);
+
+    if (l!=NULL && r!=NULL){
+        return root;
+    }
+
+    if(l != NULL){
+        return l;
+    }else{
+        return r;
+    }
+}
+
+void dist(node*root,int n1,int n2,int lvl){
+
+    if(root==NULL){
+        return;
+    }
+    if(root->data==n1 || root->data==n2){
+        distan=distan+lvl;
+    }
+    dist(root->left,n1,n2,lvl+1);
+    dist(root->right,n1,n2,lvl+1);
+
+
+}
+
+node*mirror(node*root){
+    if(root==NULL){
+        return NULL;
+    }
+    mirror(root->left);
+    mirror(root->right);
+    node*temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    return root;
+}
+
 
 void bfs(node *root){
     queue<node*> q;
@@ -146,6 +194,30 @@ void bfs(node *root){
     }
     return;
 }
+void lvl2(node *root){
+    queue<node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        int size=q.size();
+        int n=size;
+        cout<<endl;
+        while(n--){
+            node* f = q.front();
+            cout<<f->data<<",";
+            q.pop();
+
+            if(f->left){
+                q.push(f->left);
+            }
+            if(f->right){
+                q.push(f->right);
+            }
+        }
+    }
+    return;
+}
+
 
 
 int main(){
@@ -173,6 +245,20 @@ int main(){
     cout<<endl;
     cout<<"Printing Tree POSTORDER"<<endl;
     printPost(root);
+    cout<<"\nRoot to Leaf"<<endl;
+    vector<int> v;
+    root2LeafAllPaths(root,v);
+    cout<<"\nLCA of 8 and 13 is "<<LCA(root,8,13)->data<<endl;
+    node*lca=LCA(root,3,13);
+    dist(lca,3,13,0);
+    cout<<"Distance "<<distan<<endl;
+    cout<<"Diameter of Binary Tree i.e max distance "<<diameter(root)<<endl;
+    cout<<"Level order ( everylevel ) ";
+    lvl2(root);
+    node*mroot=mirror(root);
+    cout<<"\nMirror Tree"<<endl;
+    lvl2(mroot);
+
 return 0;
 }
 
